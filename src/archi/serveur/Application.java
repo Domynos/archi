43cf0a1326512b5f46109be60f611b9e53a62ec1
eclectique95470 <kevin.grosgojat@gmail.com>
2015-05-18 15:39:16 +0200

    telechargement fichiers

--- src/archi/serveur/Application.java
+++ src/archi/serveur/Application.java
@@ -1,13 +1,18 @@
 package archi.serveur;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Application {
 
@@ -17,7 +22,7 @@ public class Application {
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		File rootdir = new File("C:/www/website");
-		int port = 8282;
+		int port = 8080;
 		ServerSocket serveur = new ServerSocket(port);
 		try {
 			while(true){
@@ -29,15 +34,25 @@ public class Application {
 					PerformRequest pr = new PerformRequest(route);
 					String display = pr.getDisplay();
 					OutputStreamWriter outWriter;
+					
 					if(null == display){
+						Path path = Paths.get("C:/www/website"+route);
+						System.out.println(path);
+						byte[] data = Files.readAllBytes(path);
+						String filename = route.substring(route.lastIndexOf('/')+1, route.length());
 						outWriter = new OutputStreamWriter(socket.getOutputStream());
-						outWriter.write("HTTP/1.1 301 Found\r\n");    
+						outWriter.write("HTTP/1.1 200\r\n");    
 			            outWriter.write("Content-Type: application/octet-stream\r\n");
-			            outWriter.write("Content-Disposition: attachment; filename=\"C:/www/website"+route+"\"\r\n");
-			            outWriter.write("Location: /\r\n");
-			            outWriter.write("Connection: Close");
+			            outWriter.write("Content-Disposition: attachment; filename=\""+filename+"\"\r\n"); 
+			            outWriter.write("Content-Length:"+String.valueOf( data.length )+"\r\n");
+			            outWriter.write("Location:"+route+"\r\n");
+			            outWriter.write("Cache-Control: no-cache, must-revalidate\r\n");
+			            outWriter.write("Pragma: no-cache");
 			            outWriter.write("\r\n\r\n");
+			            outWriter.flush();
+			            sendBytes(data, socket);
 			            outWriter.close();
+
 					}
 					else{
 						PrintWriter out =
@@ -52,6 +67,26 @@ public class Application {
 		} finally{
 			serveur.close();
 		}
+	}
+	static void sendBytes(byte[] myByteArray, Socket socket) throws IOException {
+	    sendBytes(myByteArray, 0, myByteArray.length, socket);
+	}
+
+	static void sendBytes(byte[] myByteArray, int start, int len, Socket socket) throws IOException {
+	    if (len < 0)
+	        throw new IllegalArgumentException("Negative length not allowed");
+	    if (start < 0 || start >= myByteArray.length)
+	        throw new IndexOutOfBoundsException("Out of bounds: " + start);
+
+	    OutputStream out = socket.getOutputStream(); 
+	    DataOutputStream dos = new DataOutputStream(out);
+	    
+	    //dos.writeInt(len);
+	    //dos.writeLong(len);
+	    if (len > 0) {
+	        dos.write(myByteArray, start, len);
+	    }
+	    dos.flush();
 	}
 
 }
